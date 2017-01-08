1c317af6957b221b1b8d15f0a83232a5794ec9e9
Virgile Coste <Virgile.Coste@etu.udamail.fr>
2017-01-08 12:49:28 +0100

    vue de Ajout Symptome terminée (100%)

--- Hopital_1/src/controller/AjoutSymptomeController.java
+++ Hopital_1/src/controller/AjoutSymptomeController.java
@@ -5,12 +5,25 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import launch.Main;
+import modele.Symptome;
+import org.omg.CORBA.MARSHAL;
 
 /**
  * FXML Controller class
@@ -20,16 +33,58 @@ import javafx.scene.control.TextField;
 public class AjoutSymptomeController implements Initializable {
 
     @FXML
-    private ListView<?> list;
+    private ListView<Symptome> list;
     @FXML
     private TextField nom;
+    @FXML
+    private TextArea description;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        list.itemsProperty().bind(Main.getHopital().listeSymptome());
     }    
-    
+
+    @FXML
+    private void retour(ActionEvent event) throws IOException {
+        BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/ihm/Accueil.fxml"));
+        Scene scene = new Scene(root);
+        Stage st = EcranLogController.getStage();
+        st.setScene(scene);        
+        st.show();
+    }
+
+    @FXML
+    private void deconnection(ActionEvent event) throws IOException {
+        BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/ihm/EcranLog.fxml"));
+        Scene scene = new Scene(root);
+        Main.getStage().setScene(scene);
+        Main.getStage().show();
+        EcranLogController.getStage().hide();
+       
+    }
+
+    @FXML
+    private void ajouter(ActionEvent event) {
+        
+        if(nom.getText().equals("")){showMessage(Alert.AlertType.ERROR, null, "Données invalides");return;}
+        
+        Symptome s = new Symptome(nom.getText(), description.getText());
+        Main.getHopital().getListeSymptome().add(s);
+        nom.setText("");description.setText("");
+        
+    }
+   
+    private Optional<ButtonType> showMessage(Alert.AlertType type,String header,String message,ButtonType... lesBoutonsDifferents){
+        Alert laFenetre = new Alert(type);
+        laFenetre.setHeaderText(header);
+        laFenetre.setContentText(message);
+        if (lesBoutonsDifferents.length > 0) {
+            laFenetre.getButtonTypes().clear();
+            laFenetre.getButtonTypes().addAll(lesBoutonsDifferents);
+        }
+        return laFenetre.showAndWait();
+    }
 }
